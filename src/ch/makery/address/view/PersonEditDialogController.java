6dd9c3aa33f6cf1e4c994906bdd62d12d16e7cdd
rinoyrix <rinoyrix@gmail.com>
2015-08-30 16:29:38 -0700

    hahaa 3

--- src/ch/makery/address/view/PersonEditDialogController.java
+++ src/ch/makery/address/view/PersonEditDialogController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import ch.makery.address.model.Person;
 import ch.makery.address.util.DateUtil;
