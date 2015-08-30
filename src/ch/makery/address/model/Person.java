9794404380b42c331c6634dc75d0004ee92bf312
rinoyrix <rinoyrix@gmail.com>
2015-08-30 16:32:29 -0700

    hahaha 2

--- src/ch/makery/address/model/Person.java
+++ src/ch/makery/address/model/Person.java
@@ -9,10 +9,6 @@ import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
-
-import ch.makery.address.util.LocalDateAdapter;
-
 /**
  * Model class for a Person.
  *
@@ -111,7 +107,6 @@ public class Person {
         return city;
     }
 
-    @XmlJavaTypeAdapter(LocalDateAdapter.class)
     public LocalDate getBirthday() {
         return birthday.get();
     }
